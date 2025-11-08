9734a2c43c037c7eec899ff296a21db2922b54c3
P!ArteFrequencia25 <piartefrequencia@gmail.com>
2025-11-08 19:33:47 -0300

    Ajuste para o deploy railwal

--- src/main/java/com/br/artefrequencia/ApiArteFrequencia/ApiArteFrequenciaApplication.java
+++ src/main/java/com/br/artefrequencia/ApiArteFrequencia/ApiArteFrequenciaApplication.java
@@ -6,22 +6,26 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 import io.github.cdimascio.dotenv.Dotenv;
 
+
+
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ApiArteFrequenciaApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
+			// Carrega variáveis do .env se existir localmente
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing() // ignora se não existir (ex: em produção no Railway)
+				.load();
 
-		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("CORS_ORIGIN", dotenv.get("CORS_ORIGIN"));
-		System.setProperty("API_BASE_PATH", dotenv.get("API_BASE_PATH"));
+		// Seta propriedades de sistema
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST", System.getenv("DB_HOST")));
+		System.setProperty("DB_PORT", dotenv.get("DB_PORT", System.getenv("DB_PORT")));
+		System.setProperty("DB_USER", dotenv.get("DB_USER", System.getenv("DB_USER")));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD", System.getenv("DB_PASSWORD")));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME", System.getenv("DB_NAME")));
+
 		
-
 		SpringApplication.run(ApiArteFrequenciaApplication.class, args);
 
 	}
